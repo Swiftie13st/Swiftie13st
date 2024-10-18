3ab92ce15860bddaa68dc6a43ef8099c0ec45429
Bruce <swiftie13s@163.com>
2024-10-18 11:30:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const readme = `\
 
 ![](https://raw.githubusercontent.com/Swiftie13st/Swiftie13st/main/assets/github-contribution-grid-snake-dark.svg)
 
+[![wakatime](https://wakatime.com/badge/user/b3fdb6b1-f5a8-422c-9c33-4355b9b719ab.svg)](https://wakatime.com/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab)
+
 <div style="display: flex; justify-content: space-between; width: 80%; margin: 0 auto;">
     <img src="https://metrics.lecoq.io/Swiftie13st?template=classic&config.timezone=Asia%2FShanghai" style="width: 32%;" alt="1">
     <img src="https://wakatime.com/share/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab/65dc6f8c-2041-46a0-ad3b-3cfa5fc71cea.svg" style="width: 32%;" alt="2">
