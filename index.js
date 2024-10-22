5307e3ef1c773b0d90cd458811d0f6fa2c199915
Bruce <swiftie13s@163.com>
2024-10-22 15:20:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ const readme = `\
 [![wakatime](https://wakatime.com/badge/user/b3fdb6b1-f5a8-422c-9c33-4355b9b719ab.svg)](https://wakatime.com/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab)
 
 <div style="display: flex; justify-content: space-between; width: 80%; margin: 0 auto;">
-    <img src="https://metrics.lecoq.io/Swiftie13st?template=classic&config.timezone=Asia%2FShanghai" style="width: 32%;" alt="1">
-    <img src="https://wakatime.com/share/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab/65dc6f8c-2041-46a0-ad3b-3cfa5fc71cea.svg" style="width: 32%;" alt="2">
-    <img src="https://wakatime.com/share/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab/697f8652-2f65-488b-895e-4412cdfcb344.svg" style="width: 32%;" alt="3">
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=swiftie13st&layout=compact" style="width: 35%;" alt="1">
+    <img src="https://wakatime.com/share/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab/65dc6f8c-2041-46a0-ad3b-3cfa5fc71cea.svg" style="width: 26%;" alt="2">
+    <img src="https://wakatime.com/share/@b3fdb6b1-f5a8-422c-9c33-4355b9b719ab/697f8652-2f65-488b-895e-4412cdfcb344.svg" style="width: 26%;" alt="3">
 </div>
 
 
